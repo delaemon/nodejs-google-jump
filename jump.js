d8bed4c87b11775178caff19d8d4ec6dfe5cc36b
delaemon <takeshi.music.5@gmail.com>
2017-01-06 23:30:47 +0900

    Tmp2

--- jump.js
+++ jump.js
@@ -8,21 +8,15 @@ function Hash(key, numBuckets) {
     var nb = bignum(numBuckets)
     var k = bignum(key)
     var m = bignum('2862933555777941757')
-    var ab = new ArrayBuffer(8)
-    var f64a = new Float64Array(ab)
-    var ui32a = new Uint32Array(ab)
+    var i = 0
     while (nb.gt(j)) {
-        b = j
+        b = bignum(j)
         k = k.mul(m).add(1)
-        t1 = bignum(1).shiftLeft(31).toString()
-        t2 = k.shiftRight(33).add(1).toString()
-        ui32a[0] = parseInt(t1.slice(8, 16))
-        ui32a[1] = parseInt(t1.slice(0, 8))
-        t1 = f64a[0]
-        ui32a[0] = parseInt(t2.slice(8, 16))
-        ui32a[1] = parseInt(t2.slice(0, 8))
-        t2 = f64a[0]
-        j =  b.add(1).mul(t1/t2)
+        bb = b.add(1).toNumber().toFixed(8)
+        t1 = bignum(1).shiftLeft(31).toNumber().toFixed(9)
+        t2 = k.shiftRight(33).toNumber().toFixed(8)
+        j = bignum(bb * (t1/t2))
+        break
     }
 
     return b
